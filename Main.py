0a8499ec56f2869392a337fef041377cb2cfc9e2
adrian kolber <misquor@gmail.com>
2026-06-13 15:12:09 +0200

    first commit

---
+++ Main.py
@@ -0,0 +1,4 @@
+from services.openwather_api import get_weather
+
+weather_record = get_weather()
+get_weather()
---
+++ config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+
+import os
+load_dotenv()
+
+
+class Config:
+    API_KEY = os.getenv("ENV_API_KEY")
+    PLACE = os.getenv("ENV_PLACE")
